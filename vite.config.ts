37e3ce8114fd75856f8e8c4a05c5c80781a2a329
Jakub Hermyt <kubahermyt@gmail.com>
2025-10-20 10:05:59 +0200

    feat: vercel.json + vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -12,4 +11,12 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  build: {
+    outDir: "dist",
+  },
+  base: "/",
+  server: {
+    port: 5173,
+    open: true,
+  },
 });
